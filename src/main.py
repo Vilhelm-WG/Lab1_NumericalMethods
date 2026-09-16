17a2b706ef0542f0e15ccd850554da4972b94c23
Гайсюк Вілгельм 141 (2-1) <haisiuk.vilhelm@chnu.edu.ua>
2026-09-16 22:05:22 +0300

    add Трансцендентне рівняння to main

--- src/main.py
+++ src/main.py
@@ -13,3 +13,11 @@ print(f"Алгебраїчне рівняння:")
 print(f"  Корінь: {root_alg:.4f}")
 print(f"  Нев'язка: {f_alg(root_alg):.8f}")
 print(f"  Ітерацій: {result_alg.iterations}\n")
+
+# 2. Трансцендентне рівняння
+a_t, b_t = trans_interval
+root_trans, result_trans = toms748(f_trans, a=a_t, b=b_t, xtol=EPS, full_output=True)
+print(f"Трансцендентне рівняння:")
+print(f"  Корінь: {root_trans:.4f}")
+print(f"  Нев'язка: {f_trans(root_trans):.8f}")
+print(f"  Ітерацій: {result_trans.iterations}")
