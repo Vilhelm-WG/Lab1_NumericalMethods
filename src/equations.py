ce09db7d311a94cda71d7a4dbfd9d4f12e154491
Гайсюк Вілгельм 141 (2-1) <haisiuk.vilhelm@chnu.edu.ua>
2026-09-16 22:03:55 +0300

    add трансцендентне рівняння

--- src/equations.py
+++ src/equations.py
@@ -9,3 +9,13 @@ d2f_alg = lambda x: 30*x**4 - 6
 
 # Відрізок ізоляції
 alg_interval = (1.0, 2.0)
+
+# --- Трансцендентне рівняння: ln(7.62x) + 2.5 - 8.59x = 0 ---
+f_trans = lambda x: np.log(7.62 * x) + 2.5 - 8.59 * x
+
+# Перша та друга похідні
+df_trans = lambda x: 1/x - 8.59
+d2f_trans = lambda x: -1/(x**2)
+
+# Відрізок ізоляції
+trans_interval = (0.1, 1.0)
